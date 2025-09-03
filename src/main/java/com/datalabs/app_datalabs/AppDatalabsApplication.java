d66e8675c05d0f1e473038c0640174c05e551545
Stefanos Arapoglou <arapsteflol@gmail.com>
2025-09-03 20:15:43 +0300

    initial commit

---
+++ src/main/java/com/datalabs/app_datalabs/AppDatalabsApplication.java
@@ -0,0 +1,13 @@
+package com.datalabs.app_datalabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDatalabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDatalabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datalabs/app_datalabs/AppDatalabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datalabs.app_datalabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDatalabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4fffafcb6c00e0c9d9b719b3e849ba6bc3434571
Stefanos Arapoglou <arapsteflol@gmail.com>
2025-09-03 20:32:02 +0300

    initial commit

--- src/main/java/com/datalabs/app_datalabs/AppDatalabsApplication.java
+++ src/main/java/com/datalabs/app_datalabs/AppDatalabsApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppDatalabsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hey");
+		System.out.println("hey2");
 		SpringApplication.run(AppDatalabsApplication.class, args);
 	}
 
